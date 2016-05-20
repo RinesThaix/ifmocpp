96b7d4710fcd6878ba1b1df2433e1b12d581ce5c
RinesThaix <iam@kostya.sexy>
2016-05-20 07:05:09 +0300

    task7

---
+++ task7/lazy_string.cpp
@@ -0,0 +1,92 @@
+/* 
+ * File:   lazy_string.cpp
+ * Author: 0xC0deBabe <iam@kostya.sexy>
+ * 
+ * Created on 14 мая 2016 г., 11:05
+ */
+
+#include "lazy_string.h"
+#include <iostream>
+#include <stdexcept>
+#include <pthread.h>
+
+using namespace std;
+
+lazy_string::lazy_string() {
+    lock();
+    this->present = string();
+    this->start = this->sizevar = 0;
+    unlock();
+}
+
+lazy_string::lazy_string(const string &string) {
+    lock();
+    this->present = string;
+    this->start = 0;
+    this->sizevar = string.size();
+    unlock();
+}
+
+lazy_string::lazy_string(const lazy_string &ls, size_t start, size_t size) {
+    lock();
+    this->present = ls.present;
+    this->start = start;
+    this->sizevar = size;
+    unlock();
+}
+
+void lazy_string::lock() const {
+    pthread_mutex_lock(&this->sync);
+}
+
+void lazy_string::unlock() const {
+    pthread_mutex_unlock(&this->sync);
+}
+
+size_t lazy_string::size() const {
+    lock();
+    size_t result = this->sizevar;
+    unlock();
+    return result;
+}
+
+size_t lazy_string::length() const {
+    return size();
+}
+
+const char& lazy_string::at(size_t pos) const {
+    lock();
+    const char& result = this->present[this->start + pos];
+    unlock();
+    return result;
+}
+
+const char& lazy_string::operator[](size_t pos) const {
+    return at(pos);
+}
+
+lazy_string lazy_string::substr(size_t pos, size_t len) {
+    if(pos > size())
+        throw out_of_range("Can not take substring of given length from the given starting position (lazy_string)");
+    lock();
+    lazy_string result = lazy_string(this->present, this->start + pos, pos + len > size() ? (size() - pos) : len);
+    unlock();
+    return result;
+}
+
+istream& operator>>(istream &is, lazy_string &ls) {
+    ls.lock();
+    is >> ls.present;
+    ls.start = 0;
+    ls.sizevar = ls.present.size();
+    ls.unlock();
+    return is;
+}
+
+ostream& operator<<(ostream &os, lazy_string &ls) {
+    ls.lock();
+        for(size_t j = 0; j < ls.size(); ++j)
+            os << ls.present[ls.start + j];
+    ls.unlock();
+    return os;
+}
---
+++ task7/lazy_string.h
@@ -0,0 +1,105 @@
+/* 
+ * File:   lazy_string.h
+ * Author: 0xC0deBabe <iam@kostya.sexy>
+ *
+ * Created on 14 мая 2016 г., 11:05
+ */
+
+#ifndef LAZY_STRING_H
+#define	LAZY_STRING_H
+
+#include <string>
+#include <istream>
+#include <ostream>
+#include <pthread.h>
+
+using namespace std;
+
+class lazy_string {
+    
+private:
+    size_t start, sizevar;
+    string present;
+    lazy_string(const lazy_string &parent, size_t start, size_t size);
+    mutable pthread_mutex_t sync;
+    
+    void lock() const;
+    
+    void unlock() const;
+    
+public:
+    
+    /**
+     * Creates empty lazy_string.
+     */
+    lazy_string();
+    
+    /**
+     * Creates new lazy_string from the given std::string.
+     * @return lazy_string instance.
+     */
+    lazy_string(const string &string);
+    
+    /**
+     * Returns string's length (size).
+     * @return string's length (size).
+     */
+    size_t size() const;
+    
+    /**
+     * Returns string's length (size).
+     * @return string's length (size).
+     */
+    size_t length() const;
+    
+    /**
+     * Creates std::string using COW method from this lazy_string.
+     * @return std::string instance.
+     */
+    operator std::string();
+    
+    /**
+     * Returns character at specified index.
+     * @param character's index.
+     * @return character at specified index.
+     */
+    const char& at(size_t pos) const;
+    
+    /**
+     * Returns character at specified index.
+     * @param character's index.
+     * @return character at specified index.
+     */
+    const char& operator[](size_t pos) const;
+    
+    /**
+     * Returns a newly constructed lazy_string object with its value initialized to a copy of a substring of this object.
+     * @param pos position of the first character to be copied as a substring.
+     * @param len number of characters to include in the substring
+     * (if the string is shorter, as many characters as possible are used).
+     * @return A lazy_string object with a substring of this object.
+     * @throws std::out_of_range whether pos > size()
+     */
+    lazy_string substr(size_t pos = 0, size_t len = string::npos);
+    
+    /**
+     * Extracts a lazy_string from the input stream is, storing the sequence in ls,
+     * which is overwritten (the previous value of ls is replaced).
+     * @param is input stream.
+     * @param ls lazy_string.
+     * @return the same as parameter is.
+     */
+    friend istream& operator>>(istream &is, lazy_string &ls);
+    
+    /**
+     * Inserts the sequence of characters that conforms value of ls into os.
+     * @param os output stream.
+     * @param ls lazy_string.
+     * @return the same as parameter os.
+     */
+    friend ostream& operator<<(ostream &os, lazy_string &ls);
+    
+};
+
+#endif	/* LAZY_STRING_H */
+
